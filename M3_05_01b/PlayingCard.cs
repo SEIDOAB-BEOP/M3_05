5f1cdd12d1df756c4f16a3302a9311a65784e075
Martin Lenart <martin_lenart@icloud.com>
2023-06-27 19:03:28 +0200

    Add project files.

---
+++ M3_05_01b/PlayingCard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOPM3_05_01b
+{
+    class PlayingCard : IPlayingCard
+    {
+		PlayingCardColor _color;
+		public PlayingCardColor Color
+		{ 
+		  get {return _color; }
+		  init { _color = value; } 
+		}
+		public PlayingCardValue Value { get; init; }
+
+		public string BlackOrRed
+		{
+			get
+			{
+				if (Color == PlayingCardColor.Clubs || Color == PlayingCardColor.Spades)
+					return "Black";
+
+				return "Red";
+			}
+		}
+		public string StringToPrint => $"{Value} of {Color}, a {BlackOrRed} card";
+	}
+}
