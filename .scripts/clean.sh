607979d7ba90b8026f2bcce873858df4d41703ab
Martin <martin_lenart@icloud.com>
2024-08-23 11:21:00 +0200

    update

--- .scripts/clean.sh
+++ .scripts/clean.sh
@@ -2,6 +2,9 @@
 #To make the .sh file executable
 #sudo chmod +x ./clean.sh
 
-#remove all obj and bin directories within the workspace
+#remove recursively all obj and bin directories within the workspace
 cd ..
 find . -type d \( -name "obj" -o -name "bin" \) -print0 | xargs -0 rm -rf
+
+#remove recursively all csproj.bak
+find . -type f -name "*.csproj.bak" -print0 | xargs -0 rm -rf
